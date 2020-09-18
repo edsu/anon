39470b0a3adfc6339225329d60a8b20cff348c97
Ed Summers <ehs@pobox.com>
2020-09-18 12:28:36 +0000

    added some async to remove callback nesting hell

--- anon.js
+++ anon.js
@@ -122,7 +122,6 @@ async function takeScreenshot(url) {
 
   const filename = Date.now() + '.png'
   const selector = 'table.diff.diff-contentalign-left'
-  const padding = 0
   
   const rect = await page.evaluate(selector => {
     const element = document.querySelector(selector)
@@ -133,10 +132,10 @@ async function takeScreenshot(url) {
   await page.screenshot({
     path: filename,
     clip: {
-      x: rect.left - padding,
-      y: rect.top - padding,
-      width: rect.width + padding * 2,
-      height: rect.height + padding * 2
+      x: rect.left,
+      y: rect.top,
+      width: rect.width,
+      height: rect.height
     }
   })
 
@@ -144,66 +143,63 @@ async function takeScreenshot(url) {
   return filename
 }
 
-function sendStatus(account, status, edit) {
+async function sendStatus(account, status, edit) {
   console.log(status)
 
   if (!argv.noop && (!account.throttle || !isRepeat(edit))) {
 
-    takeScreenshot(edit.url).then(function(screenshot) {
+    const screenshot = await takeScreenshot(edit.url)
 
-      // Mastodon
-      if (account.mastodon) {
-        const mastodon = new Mastodon(account.mastodon)
-        mastodon.post('media', {file: fs.createReadStream(screenshot)})
-          .then(function(response) {
-            if (!response.data.id) {
-              console.log('error uploading screenshot to mastodon')
-              return
-            }
-            mastodon.post('statuses', { 'status': status, media_ids: [response.data.id] }, function(err) {
-              if (err) {
-                console.log(err)
-              }
-            })
-        })
+    // Mastodon
+    if (account.mastodon) {
+      const mastodon = new Mastodon(account.mastodon)
+      const response = await mastodon.post('media', {file: fs.createReadStream(screenshot)})
+      if (!response.data.id) {
+        console.log('error uploading screenshot to mastodon')
+        return
       }
 
-      // Twitter
-      if (account.access_token) {
-        const twitter = new Twit(account)
-        const b64content = fs.readFileSync(screenshot, {encoding: 'base64'})
+      await mastodon.post(
+        'statuses', 
+        {'status': status, media_ids: [response.data.id]},
+        err => console.log(`mastodon post failed: ${err}`)
+      )
+    }
 
-        // upload the screenshot to twitter
-        twitter.post('media/upload', {media_data: b64content}, function(err, data, response) {
+    // Twitter
+    if (account.access_token) {
+      const twitter = new Twit(account)
+      const b64content = fs.readFileSync(screenshot, {encoding: 'base64'})
+
+      // upload the screenshot to twitter
+      twitter.post('media/upload', {media_data: b64content}, function(err, data, response) {
+        if (err) {
+          console.log(err)
+          return
+        }
+
+        // add alt text for the media, for use by screen readers
+        const mediaIdStr = data.media_id_string
+        const altText = "Screenshot of edit to " + edit.page
+        const metaParams = {media_id: mediaIdStr, alt_text: {text: altText}}
+
+        twitter.post('media/metadata/create', metaParams, function(err, data, response) {
           if (err) {
-            console.log(err)
-            return
+            console.log('metadata upload for twitter screenshot alt text failed with error', err)
           }
-
-          // add alt text for the media, for use by screen readers
-          const mediaIdStr = data.media_id_string
-          const altText = "Screenshot of edit to " + edit.page
-          const metaParams = {media_id: mediaIdStr, alt_text: {text: altText}}
-
-          twitter.post('media/metadata/create', metaParams, function(err, data, response) {
+          const params = {
+            'status': status,
+            'media_ids': [mediaIdStr]
+          }
+          twitter.post('statuses/update', params, function(err) {
             if (err) {
-              console.log('metadata upload for twitter screenshot alt text failed with error', err)
+              console.log(err)
             }
-            const params = {
-              'status': status,
-              'media_ids': [mediaIdStr]
-            }
-            twitter.post('statuses/update', params, function(err) {
-              if (err) {
-                console.log(err)
-              }
-            })
-            fs.unlinkSync(screenshot)
           })
+          fs.unlinkSync(screenshot)
         })
-      }
-
-    })
+      })
+    }
   }
 }
 
@@ -272,9 +268,6 @@ function main() {
       return console.log(err)
     }
   })
-}
-
-async function getClip(page, selector, path, padding=16) {
 }
 
 if (require.main === module) {
